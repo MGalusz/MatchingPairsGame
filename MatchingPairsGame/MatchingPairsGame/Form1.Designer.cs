9e0f2d7e3f97de83bc19cf054a6708c19bc52f88
MGalusz <galuszka.mm@gmail.com>
2017-08-10 16:03:57 +0200

    done aplication

---
+++ MatchingPairsGame/MatchingPairsGame/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MatchingPairsGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(543, 527);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MatchingPairsGame/MatchingPairsGame/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingPairsGame
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        List<string> icons = new List<string>()
+            {
+                "!","!","N","N",",",",","k","k",
+                "b","b","v","v","w","w","z","z"
+            };
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+            AssignIconsToSquers();
+        }
+
+        private void AssignIconsToSquers()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLable = control as Label;
+                if (iconLable != null)
+                {
+                    int randomNumber = randomNumber = random.Next(icons.Count);
+                    iconLable.Text = icons[randomNumber];
+                    iconLable.ForeColor = iconLable.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void text1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
